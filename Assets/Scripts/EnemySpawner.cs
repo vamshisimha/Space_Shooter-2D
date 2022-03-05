fa6a349ecadc31bba7cc91d3dd1dbd8a173bc378
LAPTOP-4UDAE21C\VAMSHI <78913213+vamshisimha@users.noreply.github.com>
2022-03-05 12:00:59 +0530

    3 levels

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+	// GameObject prefabs
+	public GameObject enemyPrefab;
+	public GameObject bossPrefab;
+
+	// Private boss vars
+	GameObject bossInstance;
+	bool bossSpawned = false;
+	float bossSpawnDelay = 5f;
+
+	// Private enemy vars
+	float spawnDistance = 10f;
+	float enemyRate = 5f;
+	float nextEnemy = 1f;
+
+	public float timeUntilBoss = 10f;
+	bool bossDefeated = false;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (timeUntilBoss >= 0) {
+			// Boss timer has not expired yet
+
+			nextEnemy -= Time.deltaTime;
+			timeUntilBoss -= Time.deltaTime;
+
+			if (nextEnemy <= 0) {
+				// Spawn an enemy
+
+				nextEnemy = enemyRate;  // Set nextEnemy timer to enemyRate
+				enemyRate *= 0.9f;      // Reduce enemyRate by 10%
+
+				if (enemyRate <= 1) {
+					enemyRate = 1.25f;
+				}
+
+				Vector3 offset = Random.onUnitSphere;
+				offset.z = 0;  // Z-offset is very important for 2D
+				offset.y = 5;
+				offset = offset.normalized * spawnDistance;
+				Instantiate (enemyPrefab, transform.position + offset, Quaternion.identity);
+			}
+
+		} else if (!bossSpawned) {
+			// Boss timer has expired
+			// Add artificial delay until Boss is spawned
+			
+			bossSpawnDelay -= Time.deltaTime;
+
+			if (bossSpawnDelay <= 0 && !bossSpawned) {
+				bossSpawned = true;
+
+				Vector3 offset = Random.onUnitSphere;
+				offset.z = 0;
+				offset.y = 5;
+				offset = offset.normalized * spawnDistance;
+				bossInstance = (GameObject)Instantiate (bossPrefab, transform.position + offset, Quaternion.identity);
+			}
+		} else if (!bossDefeated && bossSpawned) {
+			// Boss has spawned
+
+			if (bossInstance == null) {
+				bossDefeated = true;
+				FindObjectOfType<GameManager> ().BossDefeated ();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour {
+
+	public GameObject bullePrefab;
+	Vector3 bulletOffset = new Vector3 (0, 0.5f, 0);
+	int bulletLayer;
+	public float fireDelay = 0.25f;
+	float cooldownTimer = 0;
+
+	AudioSource playerShootSound;
+
+	void Start() {
+		bulletLayer = gameObject.layer;
+		playerShootSound = this.GetComponent<AudioSource>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		cooldownTimer -= Time.deltaTime;
+
+		if (Input.GetButton ("Fire1") && cooldownTimer <= 0) {
+			// Shoot!
+			cooldownTimer = fireDelay;
+			playerShootSound.Play ();
+
+			Vector3 offset = transform.rotation * bulletOffset;
+
+			GameObject bulletGO = (GameObject)Instantiate (bullePrefab, transform.position + offset, transform.rotation);
+			bulletGO.layer = gameObject.layer;
+		}
+			
+	}
+}
